bee1078712121bf3e332eddca917cf5b2f0c837e
misbhven <19128043@student.curtin.edu.au>
2022-09-18 00:30:00 +0800

    Added endpoints for ServiceProvider

---
+++ ServiceProvider/Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceProvider.Controllers
+{
+    [RoutePrefix("api/calculator")]
+    public class CalculatorController : ApiController
+    {
+
+        [Route("add/{numOne}/{numTwo}")]
+        [Route("add")]
+        [HttpGet]
+        public IHttpActionResult AddTwoNumbers(int numOne, int numTwo)
+        {
+            //validate
+
+            return Ok(numOne + numTwo);
+        }
+
+        [Route("add/{numOne}/{numTwo}/{numThree}")]
+        [Route("add")]
+        [HttpGet]
+        public IHttpActionResult addThreeNumbers(int numOne, int numTwo, int numThree)
+        {
+            //validate
+            return Ok(numOne + numTwo + numThree);
+        }
+
+        [Route("mul/{numOne}/{numTwo}")]
+        [Route("mul")]
+        [HttpGet]
+        public IHttpActionResult mullTwoNumbers(int numOne, int numTwo)
+        {
+            //validate
+            return Ok(numOne * numTwo);
+        }
+
+        [Route("mul/{numOne}/{numTwo}/{numThree}")]
+        [Route("mul")]
+        [HttpGet]
+        public IHttpActionResult mullThreeNumbers(int numOne, int numTwo, int numThree)
+        {
+            //validate
+            return Ok(numOne * numTwo * numThree);
+        }
+
+    }
+}
